3c25f09dc5ecc9d7fe65df33502fbbc797e3740f
Azryy <doradokurt02@gmail.com>
2025-05-30 18:50:29 +0800

    Initial commit: Project setup with client and server dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+
+  return (
+    
+      <div >
+        
+      
+
+
+      </div>
+   
+  )
+}
+
+export default App
